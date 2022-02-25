f0498b3f6ca0a6ae549133acf1c112cec4c77f93
Irsa Ashraf <irsaashraf@linux4.cs.uchicago.edu>
2022-02-24 23:20:25 -0600

    Added murals file and created a separate cdp_file for murals

---
+++ murals.py
@@ -0,0 +1,8 @@
+class Mural:
+
+    def __init__(self, title, address, latitude, longitude):
+
+        self.title = title 
+        self.address = address
+        self.latitude = latitude
+        self.longitude = longitude

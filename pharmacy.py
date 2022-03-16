7d82cc4f6f843c16966bb7b15125a128fc4719d9
Kelley Elizabeth Sarussi <ksarussi@linux5.cs.uchicago.edu>
2022-03-16 11:07:39 -0500

    removing redundant class files

--- pharmacy.py
+++
@@ -1,9 +0,0 @@
-class Pharmacy:
-
-    def __init__(self, name, coordinates, address):
-        self.name = name 
-        self.coordinates = coordinates 
-        self.address = address 
-
-    def __str__(self):
-        return f"Name={self.name}\nAddress={self.address}\nLocation\n\tLatitude={self.coordinates[0]}\n\tLongitude={self.coordinates[1]}"
